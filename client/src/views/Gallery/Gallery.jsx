55617dd4d5bb540f15c9dae56916597613dba04a
Spencer Fasulo <spencer.fasulo@icloud.com>
2023-11-01 14:39:43 -0400

    Block looks on-theme

--- client/src/views/Gallery/Gallery.jsx
+++ client/src/views/Gallery/Gallery.jsx
@@ -4,7 +4,7 @@ import GalleryItem from "./GalleryItem";
 
 
 const Gallery = () => (
-    <div className='container nav-padding'>
+    <div className='flex flex-column'>
         <NavBar />
         <h1>Gallery</h1>
         <GalleryItem />

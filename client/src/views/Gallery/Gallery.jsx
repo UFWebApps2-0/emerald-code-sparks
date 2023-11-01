f6799ef31f084b98061476b372594887ef289ecf
Spencer Fasulo <spencer.fasulo@icloud.com>
2023-11-01 14:16:57 -0400

    Empty gallery items

--- client/src/views/Gallery/Gallery.jsx
+++ client/src/views/Gallery/Gallery.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import Logo from "../../assets/casmm_logo.png";
 import NavBar from "../../components/NavBar/NavBar";
+import GalleryItem from "./GalleryItem";
+
 
 const Gallery = () => (
     <div className='container nav-padding'>
         <NavBar />
-        <div id='join-wrapper'>
-            <img src={Logo} id='casmm-logo' alt='logo'/>
-        </div>
+        <GalleryItem />
     </div>
 )
 
---
+++ client/src/views/Gallery/GalleryItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './GalleryItem.less';
+
+
+const GalleryItem = () => (
+    <div className='container nav-padding'>
+        <div className='galleryItem'>
+
+        </div>
+    </div>
+)
+
+export default GalleryItem;

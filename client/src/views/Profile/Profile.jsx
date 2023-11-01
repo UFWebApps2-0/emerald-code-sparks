71ca0ae09082f26d81c3e56ce0113dec412d41f2
Exanite <exanite78012@gmail.com>
2023-11-01 11:56:09 -0400

    Begin implementing user profile card

--- client/src/views/Profile/Profile.jsx
+++ client/src/views/Profile/Profile.jsx
@@ -1,6 +1,18 @@
+import "./Profile.less";
+
 const Profile = () => {
   return (
-    <h1>Hello world!</h1>
+    <>
+      <div className={"profile-card"}>
+        <img
+          className={"profile-card-image"}
+          src="https://media.discordapp.net/attachments/1163871360972501144/1168945792451629216/image.png"
+          alt="User profile"
+        />
+        <h1 className={"profile-card-name"}>User name</h1>
+        <p className={"profile-card-name"}>User role</p>
+      </div>
+    </>
   )
 }
 

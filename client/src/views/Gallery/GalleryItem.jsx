68ea36549fbc9297de0d0c0e4f05d483aba7b816
Spencer Fasulo <spencer.fasulo@icloud.com>
2023-11-01 14:39:43 -0400

    Block looks on-theme

--- client/src/views/Gallery/GalleryItem.jsx
+++ client/src/views/Gallery/GalleryItem.jsx
@@ -4,8 +4,10 @@ import './GalleryItem.less';
 
 const GalleryItem = () => (
     <div className='container nav-padding'>
+        <div className='flex flex-row align-center justify-center'>
         <div className='galleryItem'>
-            
+            <div className='header'><div>Project Name</div></div>
+        </div>
         </div>
     </div>
 )

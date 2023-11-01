1b1da7ea29533a0afa327a3b4af9f357ebf4e7d2
Spencer Fasulo <spencer.fasulo@icloud.com>
2023-11-01 14:19:55 -0400

    Red box wow

--- client/src/views/Gallery/Gallery.jsx
+++ client/src/views/Gallery/Gallery.jsx
@@ -6,6 +6,7 @@ import GalleryItem from "./GalleryItem";
 const Gallery = () => (
     <div className='container nav-padding'>
         <NavBar />
+        <h1>Gallery</h1>
         <GalleryItem />
     </div>
 )

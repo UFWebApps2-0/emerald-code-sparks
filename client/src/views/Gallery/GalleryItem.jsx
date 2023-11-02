9173eabf0ca8ada4b3ef5217d5d6eb6f82a8178c
sfasulo <spencerf0123@gmail.com>
2023-11-01 22:56:09 -0400

    Add other items to gallery scaffold

--- client/src/views/Gallery/GalleryItem.jsx
+++ client/src/views/Gallery/GalleryItem.jsx
@@ -5,9 +5,21 @@ import './GalleryItem.less';
 const GalleryItem = () => (
     <div className='container nav-padding'>
         <div className='flex flex-row align-center justify-center'>
-        <div className='galleryItem'>
-            <div className='header'><div>Project Name</div></div>
-        </div>
+            <div className='galleryItem'>
+                <div className='header'><div>Project Name</div></div>
+                <img style={{ backgroundColor: 'red' }} />
+                <div className='flex flex-row'>
+                    <div className='flex flex-column'>
+                        <p>Creator:</p>
+                        <p>Creator Name</p>
+                        <p>Posted:</p>
+                        <p>Posted Date</p>
+                    </div>
+                    <div className='flex flex-column justify-end'>
+                        <p>7  5</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 )

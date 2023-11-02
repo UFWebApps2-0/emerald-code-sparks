46b6f09f9b58af6f39921a694f03760f8dff55e6
mmodrono <mannymodrono03@gmail.com>
2023-11-02 13:42:34 -0400

    Started styling

--- client/src/views/Mentor/Classroom/Discussion/Discussion.jsx
+++ client/src/views/Mentor/Classroom/Discussion/Discussion.jsx
@@ -11,8 +11,40 @@ const Discussion = () => {
   };
 
   return (
-    <div className='discussionBoard'>
-      <h1>Discussion Board</h1>
+    <div
+      style={{
+        margin: 'auto',
+        padding: '5vh 5vw',
+        width: '80vw',
+        height: 'auto',
+        background: 'rgb(233, 233, 233)',
+        borderRadius: '5px',
+        border: '2px solid #5BABDE',
+      }}
+      className='discussionBoard'
+    >
+      <h1
+        style={{
+          marginBottom: 0,
+          float: 'left',
+          position: 'relative',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: '#FFFFFF',
+          height: 'auto',
+          padding: '10px 5px',
+          width: '34%',
+          borderRadius: '80px',
+          background: '#5BABDE',
+          fontSize: '1.2em',
+          top: '5vh',
+          left: '-2vw',
+          zIndex: 1,
+        }}
+      >
+        Discussion Board
+      </h1>
       <NewPost addPost={addPost} />
       {posts.map((post, index) => (
         <Post key={index} text={post} />

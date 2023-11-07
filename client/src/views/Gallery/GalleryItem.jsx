698e04ca8b365d67f4f94f967fd960fbedf38ee9
elilowe1 <elijah.lowe@ufl.edu>
2023-11-07 18:00:24 -0500

    Fix conflict

--- client/src/views/Gallery/GalleryItem.jsx
+++ client/src/views/Gallery/GalleryItem.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { Modal, Button } from 'antd';
-import { createPortal } from 'react-dom';
-//import ExpandedGalleryItem from './ExpandedGalleryItem';
 import './GalleryItem.less';
 
 //Wrapper item needs to be a useState for it to get dynamically rendered

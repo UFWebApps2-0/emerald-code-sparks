c6ebf1ae29f2c73fc86181e9b8780891a219a292
Kyle Hoang <kyle.hoang@ufl.edu>
2023-11-07 11:34:30 -0500

    Sprint 1 Work

--- client/src/views/Student/Student.jsx
+++ client/src/views/Student/Student.jsx
@@ -9,6 +9,34 @@ function Student() {
   const [learningStandard, setLessonModule] = useState({});
   const navigate = useNavigate();
 
+  let permission = Notification.permission;
+  if(permission === "granted") {
+    showNotification();
+  } else if(permission === "default"){
+    requestAndShowPermission();
+  } else {
+    alert("Use normal alert");
+  }
+
+  function showNotification() {
+    var title = "JavaScript Jeep";
+    var icon = "image-url"
+    var body = "Message to be displayed";
+    var notification = new Notification(title, { body, icon });
+    notification.onclick = () => { 
+           notification.close();
+           window.parent.focus();
+    }
+ }
+
+ function requestAndShowPermission() {
+  Notification.requestPermission(function (permission) {
+     if (permission === "granted") {
+           showNotification();
+     }
+  });
+}
+
   useEffect(() => {
     const fetchData = async () => {
       try {

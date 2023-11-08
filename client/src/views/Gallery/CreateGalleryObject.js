8881eed9ee5bdf96d5bc334ed6185d602c74e069
AndreaC02 <andrea.chacon@ufl.edu>
2023-11-08 14:15:28 -0500

    Gallery backend finally working, can post!

--- client/src/views/Gallery/CreateGalleryObject.js
+++ client/src/views/Gallery/CreateGalleryObject.js
@@ -3,20 +3,23 @@ import { postGalleryObject } from '../../Utils/requests';
 async function createGalleryObject(title, user_name, like_count, view_count, visibility, type) {
 
   //**********create default discussion board here
-  const discussion_board = ['test'];
+  const discussion_board = [];
 
-  const data = {
-    title,
-    user_name,
-    like_count,
-    view_count,
-    discussion_board, // Make sure this variable is defined
-    type,
-    visibility,
+    const data = {
+    Title: title, 
+    User_name: user_name,
+    like_count: like_count,
+    view_count: view_count,
+    discussion_board: discussion_board,
+    visibility: visibility,
+    type: type,
   };
 
+  console.log(data)
+
  try {
-      const response = await postGalleryObject(JSON.stringify(data));
+      console.log("please omg", data)
+      const response = await postGalleryObject(data);
 
       if (response.status === 201) {
         console.log('Gallery object created successfully in the backend.');

367d5853ee7e9b368b2fd0fd58619a765613cf67
Spencer Fasulo <spencer.fasulo@icloud.com>
2023-11-09 11:40:48 -0500

    Change column placement to display correct

--- client/src/views/Gallery/Gallery.jsx
+++ client/src/views/Gallery/Gallery.jsx
@@ -7,50 +7,48 @@ import FilterComponent from './FilterComponent';
 import testings from "./testing";
 
 const Gallery = () => {
-	
-	 const [filterText, setFilterText] = useState("");
 
-  function filterUpdate(value) {
-    setFilterText(value);
-  }
-	
-	let testing= testings;
-	 
-  
-  testing = testing.filter((records) => {
-    return records.User_name.toLowerCase().includes(filterText.toLowerCase()) || records.Title.toLowerCase().includes(filterText.toLowerCase());
-  });
-  
-  
-  
+    const [filterText, setFilterText] = useState("");
 
-//Detect if the project is clicked. 
-//should change the red block to the img of galleryItem in the future.
-  const recordList = testing.map((directory) => {
-    return (		  
-						<div key={directory.Id}>
-						<GalleryItem item={directory}
-						Id = {directory.Id}
-						
-						/>
-						</div>
-    );
-  });
-  
-  
-  
-	
+    function filterUpdate(value) {
+        setFilterText(value);
+    }
+
+    let testing = testings;
+
+
+    testing = testing.filter((records) => {
+        return records.User_name.toLowerCase().includes(filterText.toLowerCase()) || records.Title.toLowerCase().includes(filterText.toLowerCase());
+    });
+
+
+
+
+    //Detect if the project is clicked. 
+    //should change the red block to the img of galleryItem in the future.
+    const recordList = testing.map((directory) => {
+        return (
+            <div key={directory.Id}>
+                <GalleryItem item={directory}
+                    Id={directory.Id}
+
+                />
+            </div>
+        );
+    });
+
+
+
+
     return (
         <>
             <NavBar />
             <div className='container nav-padding'>
                 <h1>Gallery</h1>
-                <SearchBar filterUpdate={filterUpdate}/>
+                <SearchBar filterUpdate={filterUpdate} />
                 <div className='flex flex-row'>
-                    <FilterComponent/>
-                    <div className='flex flex-column'>
-                        {recordList}
-                    </div>
+                    <FilterComponent />
+                    {recordList}
                 </div>
             </div>
         </>

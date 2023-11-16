52fee81c0dcee4754ee2632b48dea0558f8416b0
Spencer Fasulo <spencer.fasulo@icloud.com>
2023-11-15 19:22:12 -0500

    Fix searching with server-pulled components

--- client/src/views/Gallery/Gallery.jsx
+++ client/src/views/Gallery/Gallery.jsx
@@ -4,68 +4,47 @@ import GalleryItem from "./GalleryItem";
 import SearchBar from './Search';
 import FilterComponent from './FilterComponent';
 //testing GalleryItems
-import testings from "./testing";
 import { getGalleryObjects } from '../../Utils/requests';
 
 
 const Gallery = () => {
 
-    const [filterText, setFilterText] = useState("");
+    function filterUpdate(value) {
+        const filteredGalleryItems = loadedGalleryItems?.filter((item) => {
+            return item.props.Title.toLowerCase().includes(value.toLowerCase()) || item.props.User_name.toLowerCase().includes(value.toLowerCase());
+        });
+        setRenderedGalleryItems(filteredGalleryItems);
+    }
+
+    /*The gallery page will need to have lazy loading implemented
+    Therefore, we need to store ALL gallery objects, all which have been loaded (for searching), 
+    and all which are being rendered
+    */
     const [galleryObjects, setGalleryObjects] = useState(undefined);
+    const [renderedGalleryItems, setRenderedGalleryItems] = useState(undefined);
+    const [loadedGalleryItems, setLoadedGalleryItems] = useState(undefined);
 
     useEffect(() => {
         getGalleryObjects().then((response) => {
             setGalleryObjects(response.data);
+            //Convert gallery objects into JSX gallery items
+            //x is the max number of gallery items to display on load
+            const x = Math.min(12, response.data?.length || 0);
+            let tempItems = [];
+            try {
+                for (let i = 0; i < x; i++) {
+                    const it = response.data[i];
+                    tempItems.push(<GalleryItem key={it.id} Title={it.Title} User_name={it.User_name} like_count={it.like_count} view_count={it.view_count} posted={it.updated_at} />);
+                }
+            } catch (e) {
+                console.log("Error in gallery objects");
+                console.log(e);
+                console.log(tempItems);
+            }
+            setLoadedGalleryItems(tempItems);
+            setRenderedGalleryItems(tempItems);
         });
     }, []); // Empty dependency array means this effect runs once when the component mounts
-
-    function filterUpdate(value) {
-        setFilterText(value);
-    }
-
-    let testing = testings;
-
-    const [galleryItems, setGalleryItems] = useState(undefined);
-    window.galleryObjects = galleryObjects;
-
-    const x = Math.min(12, galleryObjects?.length || 0);
-
-    useEffect(() => {
-        let tempItems = [];
-        try {
-            for (let i = 0; i < x; i++) {
-                const it = galleryObjects[i];
-                tempItems.push(<GalleryItem key={it.id} Title={it.Title} User_name={it.User_name} like_count={it.like_count} view_count={it.view_count} posted={it.updated_at} />);
-            }
-        } catch (e) {
-            console.log("Not enough items in galleryItems");
-            console.log(e);
-            console.log(tempItems);
-        }
-        setGalleryItems(tempItems);
-    }, []);
-
-
-
-    testing = testing.filter((records) => {
-        return records.User_name.toLowerCase().includes(filterText.toLowerCase()) || records.Title.toLowerCase().includes(filterText.toLowerCase());
-    });
-
-    //Detect if the project is clicked. 
-    //should change the red block to the img of galleryItem in the future.
-    const recordList = testing.map((directory) => {
-        return (
-            <div key={directory.Id}>
-                <GalleryItem item={directory}
-                    Id={directory.Id}
-
-                />
-            </div>
-        );
-    });
-
-
-
 
     return (
         <>
@@ -75,7 +54,7 @@ const Gallery = () => {
                 <SearchBar filterUpdate={filterUpdate} />
                 <div className='flex flex-row'>
                     <FilterComponent />
-                    {galleryItems}
+                    {renderedGalleryItems}
                 </div>
             </div>
         </>

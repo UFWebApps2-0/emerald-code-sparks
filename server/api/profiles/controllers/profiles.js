47a6d96dfcbe793ab250e11ed7e2bb1041c74591
Exanite <exanite78012@gmail.com>
2023-11-16 13:37:02 -0500

    Begin implementing find profile endpoint

--- server/api/profiles/controllers/profiles.js
+++ server/api/profiles/controllers/profiles.js
@@ -1,8 +1,58 @@
 'use strict';
 
+const {sanitizeEntity} = require("strapi-utils");
+const { createCoreController } = require('@strapi/strapi').factories;
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = createCoreController('api::profile.profile', ({ strapi }) =>  ({
+  async find(ctx) {
+    const {id, student} = ctx.request.body
+
+    let profile = await (() => {
+      if (student) {
+        return strapi.services.profiles.find({
+          student: id,
+          type: "student",
+        }, [])
+      } else {
+        return strapi.services.profiles.find({
+          user: id,
+          type: "user",
+        }, [])
+      }
+    })()
+
+    if (!profile && false) {
+      if (student) {
+        const student = await strapi.services.student.find({
+          id: id,
+        });
+
+        if (!student) {
+          // Todo 404
+
+          return;
+        }
+      } else {
+        const user = await strapi.services.users.find({
+          id: id,
+        });
+      }
+
+      profile = await strapi.services.profiles.create({
+        name: student.name,
+        character: student.character,
+        classroom: classroom
+      })
+    }
+
+    // return the students and the current session
+    return {
+      profile: sanitizeEntity(profile),
+    }
+  },
+}));
--- server/api/profiles/services/profiles.js
+++ server/api/profiles/services/profiles.js
@@ -5,15 +5,4 @@
  * to customize this service
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::profile.profile', ({ strapi }) =>  ({
-  // Method 1: Creating an entirely custom action
-  async exampleAction(ctx) {
-    try {
-      ctx.body = 'ok';
-    } catch (err) {
-      ctx.body = err;
-    }
-  },
-}));
+module.exports = {}

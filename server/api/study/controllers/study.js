951b7ba59e7e3433b23d361ffe080805c6ae54ca
Chaitra Peddireddy <cpeddireddy@ufl.edu>
2023-11-25 08:13:59 -0500

    Conset field for study obj

--- server/api/study/controllers/study.js
+++ server/api/study/controllers/study.js
@@ -5,4 +5,23 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    //add functionality to create a new study and save it in strapi
+    async create(ctx) {
+        const { user } = ctx.state;
+        const { session } = user;
+        const { classroom, lesson_module } = ctx.request.body;
+        const { id: userId } = user;
+        const { id: sessionId } = session;
+        const { id: classroomId } = classroom;
+        const { id: lessonModuleId } = lesson_module;
+        const { id: studyId } = await strapi.services.study.create({
+            user: userId,
+            session: sessionId,
+            classroom: classroomId,
+            lesson_module: lessonModuleId,
+        });
+        return { id: studyId };
+    },
+    
+};

c7e54db0a919f28a9f673330667a6da557bef180
“KaelanL <kaelan.lux@ufl.edu>
2023-11-25 14:43:59 -0500

    pushing addition of google sign in for student login page. uses id [46] Tim J. & joinCode 2017.

--- client/src/views/Home/HomeJoin.jsx
+++ client/src/views/Home/HomeJoin.jsx
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { message } from 'antd';
 import './Home.less';
-import { getStudents } from '../../Utils/requests';
+import { getStudents, postJoin } from '../../Utils/requests';
+import { setUserSession } from '../../Utils/AuthRequests';
+import {GoogleLogin} from 'react-google-login';
+import {gapi} from 'gapi-script';
+
+const CLIENT_ID = "296846904571-jiau68kb1m5ovbjodmho8ei6fe69qbkv.apps.googleusercontent.com";
+const API_KEY = "AIzaSyBH4GlSHNm7zUcrcINb-uKI82l36vbD4jA";
+const SCOPES = "https://www.googleapis.com/auth/drive";
 
 export default function HomeJoin(props) {
   const [loading, setLoading] = useState(false);
@@ -23,25 +30,80 @@ export default function HomeJoin(props) {
     });
   };
 
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        scope: SCOPES
+      })
+    };
+  
+    gapi.load('client:auth2', start);
+  })
+  
+  const handleStudentGoogleLogin = async () => {
+    let ids = [];
+    ids[0] = 46;
+    const joinCode = 2017;
+    //console.log(ids);
+    const res = await postJoin(joinCode, ids);
+    if (res.data) {
+      setUserSession(res.data.jwt, JSON.stringify(res.data.students));
+      navigate('/student');
+    }
+    else {
+      message.error('Google Login Failed.');
+    }
+  };
+  
+  const onSucc = (res) => {
+    console.log(res);
+    handleStudentGoogleLogin();
+  };
+  
+  const onFail = (res) => {
+    console.log(res);
+  };
+  
+  function Login() {
+    return (
+        <div id="signInButton">
+          <GoogleLogin
+            className="googleButton"
+            clientID={CLIENT_ID}
+            buttonText="Google Sign-up"
+            onSuccess={onSucc}
+            onFailure={onFail}
+            cookiePolicy={'single_host_origin'}
+            isSignedIn={false}
+          />
+        </div>
+    )
+  }
+
   return (
-    <div
-      id='box'
-      onKeyPress={(e) => {
-        if (e.key === 'Enter') handleLogin();
-      }}
-    >
-      <input
-        type='text'
-        value={joinCode}
-        placeholder='Join Code'
-        onChange={(e) => setJoinCode(e.target.value)}
-      />
-      <input
-        type='button'
-        value={loading ? 'Loading...' : 'Join'}
-        onClick={handleLogin}
-        disabled={loading}
-      />
+    <div>
+      <div
+        id='box'
+        onKeyPress={(e) => {
+          if (e.key === 'Enter') handleLogin();
+        }}
+      >
+        <input
+          type='text'
+          value={joinCode}
+          placeholder='Join Code'
+          onChange={(e) => setJoinCode(e.target.value)}
+        />
+        <input
+          type='button'
+          value={loading ? 'Loading...' : 'Join'}
+          onClick={handleLogin}
+          disabled={loading}
+        />
+      </div>
+      <Login/>
     </div>
   );
 }

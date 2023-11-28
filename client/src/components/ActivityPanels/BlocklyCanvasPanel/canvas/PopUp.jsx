f1cd527e7ff93d78342b04d697445ec61bb1cde1
Grillio <bggriller@gmail.com>
2023-11-27 20:23:13 -0500

    Fixed modal tag

--- client/src/components/ActivityPanels/BlocklyCanvasPanel/canvas/PopUp.jsx
+++ client/src/components/ActivityPanels/BlocklyCanvasPanel/canvas/PopUp.jsx
@@ -1,9 +1,9 @@
 // CodePopup.js
 import React, { useState } from 'react';
-import Modal from 'react-modal';
+//import Modal from 'react-modal';
 import '../../ActivityLevels.less';
 
-Modal.setAppElement('#root'); // Set the root element for accessibility
+ // Set the root element for accessibility
 // This is code for the Pop-up that allows you to write code into
 const CodePopup = ({ isOpen, onClose, onSubmit }) => {
     const [code, setCode] = useState('void custom(){\n\t// Write Here\n\t\n} custom();');
@@ -20,14 +20,14 @@ const CodePopup = ({ isOpen, onClose, onSubmit }) => {
     };
 
     return (
-        <Modal isOpen={isOpen} onRequestClose={onClose}>
+        <div isOpen={isOpen} onRequestClose={onClose}>
             <button className="close-button" onClick={onClose}>X</button>
             <h2 className="center-text">Enter Your Code</h2>
             <textarea className="code-textarea textarea-style" value={code}
                       onChange={handleInputChange} placeholder="Write Code here"
             />
             <button className="submit-button" onClick={handleSubmit}>Submit</button>
-        </Modal>
+        </div>
     );
 };
 

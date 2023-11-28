044f1225c3ef3b20d959d44084b09e1a6177ad84
SharonHg <44092503+SharonHg@users.noreply.github.com>
2023-11-28 06:30:26 -0500

    Fork.jsx modified
    pass xml_text to /workspace

--- client/src/components/Gallery/Fork.jsx
+++ client/src/components/Gallery/Fork.jsx
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom"
-//import { Route, Routes } from 'react-router-dom';
-//import BlocklyPage from '../BlocklyPage/BlocklyPage';
-//import PrivateRoute from '../../Utils/PrivateRoute'
-
 
 function Fork({ props }) {
 
@@ -12,31 +8,12 @@ function Fork({ props }) {
 	const navigate = useNavigate();
 
 	function handleFork(e) {
-		//navigate to  /sandbox
-		//Should pass xml to the sandbox
-		navigate('/sandbox');
+		//navigate to  /workspace
+		//Should pass xml to the workspace
+		localStorage.setItem('my-activity', JSON.stringify(props.galleryObject.xml_text));
+		navigate('/workspace');
 	}
 
-	/*
-	//form Student.jsx probably not useful 
-	  const [learningStandard, setLessonModule] = useState({});
-	
-	useEffect(() => {
-	const fetchData = async () => {
-	  try {
-		const res = await getStudentClassroom();
-		if (res.data) {
-		  if (res.data.lesson_module) {
-			setLessonModule(res.data.lesson_module);
-		  }
-		} else {
-		  message.error(res.err);
-		}
-	  } catch {}
-	};
-	fetchData();
-  }, []);
-	*/
 	return (
 
 		<button className="fork-button" onClick={(e) => { handleFork(e) }}>

efefd83519389ad887d743d18ef046cdc0dbe37c
JulieCortese <117322305+JulieCortese@users.noreply.github.com>
2023-12-10 19:33:30 -0500

    removed some comments that i didn't like

--- client/src/components/Moderation/WrenchButton.jsx
+++ client/src/components/Moderation/WrenchButton.jsx
@@ -1,9 +1,5 @@
 import React, {useState} from 'react';
 import './WrenchButton.less';
-//import './ActionButtons.jsx';
-//import './ActionButtons.less';
-
-//import { <-- Import database getter/setters necessary for button function
 
 import { Button } from 'antd';
 
@@ -35,17 +31,6 @@ const Sidebar = ({uniqueKey, handleClose }) => {
 
 export default function WrenchButton({uniqueKey}) {
   const [showSidebar, setSidebar] = useState(false);
-  //let [status, setStatus] = useState("Unclicked");
-  //let [clicked, setClicked] = useState(false);
-
-  /*function Wrench() {
-    // Mark content as appropriate and remove any restrictions on it
-    alert("The content has been approved! Unique key: " + uniqueKey);
-    //can I have just one click, but the click makes the approve/reject buttons pop up?
-    // Use setter from requests.js to update status column as approved
-    // Remove hidden status from gallery, if hidden
-  }*/
-  /*  I don't know if I need an unwrench function, so we'll see.
   function UnWrench() {
     // Mark content as inappropriate and restrict it from being displayed publically
     alert("The content has been rejected! Unique key: " + uniqueKey);
